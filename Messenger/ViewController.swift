a9a4b6d39eb6771cd94e0b3cbd164c2cee5fc5f8
iosdevIslombek <islomjonrazzoqov@gmail.com>
2022-09-04 12:42:56 +0500

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Islomjon on 04/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
